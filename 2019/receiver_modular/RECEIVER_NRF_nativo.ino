a7b27b8f25014736bd108d574f706d0c408afb70
Martin Kondra <martinkondra@gmail.com>
2019-03-21 20:01:23 -0300

    Agrego nueva versión del receiver modularizada. Compila pero está sin testear, tiene un fix teórico del bug en el sampler y unas fc que van a servir para implementar el quantizer con midiClock

---
+++ 2019/receiver_modular/RECEIVER_NRF_nativo.ino
@@ -0,0 +1,199 @@
+#include "MIDIUSB.h"
+#include <SPI.h>
+#include "RF24.h"
+
+int roleSET = 0;
+bool radioNumber = roleSET;
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(5,6);
+byte addresses[][6] = {"1Node","2Node"};
+// Used to control whether this node is sending or receiving
+bool role = roleSET;
+int inInt;
+
+#include "CaxixiConfig.h"
+#include "CxCircularBuffer.h"
+
+char inData[10];
+int index;
+boolean started = false;
+boolean ended = false;
+int ch;
+int num;
+int LED_ON = 230; //Se enciende un 10% el REC_LED indicando encendido
+
+int cxRightForwardNote = CAXIXI_RIGHT_NOTE_FORWARD;
+int cxRightBackwardNote = CAXIXI_RIGHT_NOTE_BACKWARD;
+int cxRightHitNote = CAXIXI_RIGHT_NOTE_HIT;
+int cxLeftForwardNote = CAXIXI_LEFT_NOTE_FORWARD;
+int cxLeftBackwardNote = CAXIXI_LEFT_NOTE_BACKWARD;
+int cxLeftHitNote = CAXIXI_LEFT_NOTE_HIT;
+int currentOctave = 0;
+
+#include "leds.h"
+#include "Sampler.h"
+
+void setup() {
+  pinMode(RECORD_LED_PIN, OUTPUT);// initialize the LED as an output:
+  pinMode(OCTAVE_UP_LED_RED_PIN, OUTPUT);
+  pinMode(OCTAVE_UP_LED_GREEN_PIN, OUTPUT);
+  pinMode(OCTAVE_DOWN_LED_RED_PIN, OUTPUT);
+  pinMode(OCTAVE_DOWN_LED_GREEN_PIN, OUTPUT);
+  analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+  analogWrite(OCTAVE_DOWN_LED_GREEN_PIN, 255);
+  analogWrite(OCTAVE_UP_LED_RED_PIN, 255);
+  analogWrite(OCTAVE_UP_LED_GREEN_PIN, 255);
+  analogWrite(RECORD_LED_PIN, LED_ON); 
+  delay(500);
+  Serial.begin(9600); 
+  delay(100);
+  initSamplerBuffer();
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+  }else{
+    radio.openReadingPipe(1,addresses[1]);
+    radio.startListening(); 
+  }
+  delay(100);
+}
+
+void loop() {
+  time = millis()-(t0 + t1 + reset);
+  setReset();
+  PlayBuffer();
+  showLeds();
+  if( radio.available()){                      // While there is data ready
+    radio.read( &inInt, sizeof(int) ); 
+    if (inInt>999) {  //It means that its a CCMessagge
+      SplitCCM(inInt);
+      sendMIDIccm(MIDI_CHANNEL, ch, num);
+    } else if (inInt == CAXIXI_SAMPLER_CLEAR){
+      Clear();
+    } else if (inInt == CAXIXI_RECORD_START){
+      RecordStart();
+    } else if (inInt == CAXIXI_RECORD_STOP){
+      RecordStop();
+    } else if (inInt == CAXIXI_OCTAVE_UP){
+      OctaveUp();
+    } else if (inInt == CAXIXI_OCTAVE_DOWN){
+      OctaveDown();
+    } else if (inInt == CAXIXI_RIGHT_FORWARD_NOTEON){
+      SendNoteOn(cxRightForwardNote);
+    } else if (inInt == CAXIXI_RIGHT_FORWARD_NOTEOFF){
+      SendNoteOff(cxRightForwardNote);
+    } else if (inInt == CAXIXI_RIGHT_BACKWARD_NOTEON){
+      SendNoteOn(cxRightBackwardNote);
+    } else if (inInt == CAXIXI_RIGHT_BACKWARD_NOTEOFF){ 
+      SendNoteOff(cxRightBackwardNote);
+    } else if (inInt == CAXIXI_RIGHT_HIT_NOTEON){ 
+      SendNoteOn(cxRightHitNote);
+    } else if (inInt == CAXIXI_RIGHT_HIT_NOTEOFF){  
+      SendNoteOff(cxRightHitNote);
+    } else if (inInt == CAXIXI_LEFT_FORWARD_NOTEON){
+      SendNoteOn(cxLeftForwardNote);
+    } else if (inInt == CAXIXI_LEFT_FORWARD_NOTEOFF){
+      SendNoteOff(cxLeftForwardNote);
+    } else if (inInt == CAXIXI_LEFT_BACKWARD_NOTEON){ 
+      SendNoteOn(cxLeftBackwardNote);
+    } else if (inInt == CAXIXI_LEFT_BACKWARD_NOTEOFF){  
+      SendNoteOff(cxLeftBackwardNote);
+    } else if (inInt == CAXIXI_LEFT_HIT_NOTEON){  
+      SendNoteOn(cxLeftHitNote);
+    } else if (inInt == CAXIXI_LEFT_HIT_NOTEOFF){ 
+      SendNoteOff(cxLeftHitNote);
+    }
+  }
+}
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+  MidiUSB.flush();
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+  MidiUSB.flush();
+}
+
+//Probar estos cambios para que deje de grabar cuando completa el buffer!
+void SendNoteOn(int note) {
+  note = note + (currentOctave * 12);
+  if(record && !isSetT0 && FirstNote){
+    FirstNote = true;
+    setT0();
+  if(record && bufferI<SAMPLER_BUFFER_SIZE/2)
+    time = millis()-(t0 + t1 + reset);  //Esto es para que a la primera nota la guarde con time == 0
+    Buffer sample = {note, layer, 1, time};
+    samples[bufferI] = sample;
+  }
+  noteOn(MIDI_CHANNEL,note,127);
+  if(record && bufferI<SAMPLER_BUFFER_SIZE/2){
+    bufferI++;  
+  }
+}
+
+void SendNoteOff(int note) {
+  note = note + (currentOctave * 12);
+  if(record && bufferI<=SAMPLER_BUFFER_SIZE/2){
+    Buffer sample = {note, layer, 0, time};
+    samples[bufferI] = sample;
+  }
+  noteOff(MIDI_CHANNEL,note,127);
+  if(record && bufferI<=SAMPLER_BUFFER_SIZE/2){
+    bufferI++;  
+  }
+}
+
+void sendMIDIccm(byte channel, byte control, byte value){//ch=controller num=value
+      midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+      MidiUSB.sendMIDI(event);
+      MidiUSB.flush();
+    }
+
+void SplitCCM(int inInt){
+    ch = inInt/1000;
+    num = inInt - ch*1000;
+}
+
+void PlayBuffer() {
+  if(play){
+    if(samples[bufferJ].time-time<0 && samples[bufferJ].time-time>-100){//Esto es un hardcodeo (el -100), corregirlo mirando el reloj en la V2, o dejarlo...
+      if(samples[bufferJ].encendido){
+       noteOn(MIDI_CHANNEL,samples[bufferJ].note,127);
+      }else{
+       noteOff(MIDI_CHANNEL,samples[bufferJ].note,127);
+      }
+      bufferJ++; //J se reinicia cuando da la vuelta, en la fc setReset 
+    }
+  }
+}
+
+void OctaveUp() {
+  SendNoteOff(cxRightForwardNote);
+  SendNoteOff(cxRightBackwardNote);
+  SendNoteOff(cxRightHitNote);
+  SendNoteOff(cxLeftForwardNote);
+  SendNoteOff(cxLeftBackwardNote);
+  SendNoteOff(cxLeftHitNote);
+  currentOctave = currentOctave + 1;
+  if (currentOctave>3) {
+    currentOctave = -3;
+  }
+}
+
+void OctaveDown() {
+  SendNoteOff(cxRightForwardNote);
+  SendNoteOff(cxRightBackwardNote);
+  SendNoteOff(cxRightHitNote);
+  SendNoteOff(cxLeftForwardNote);
+  SendNoteOff(cxLeftBackwardNote);
+  SendNoteOff(cxLeftHitNote);
+  currentOctave = currentOctave - 1;
+  if (currentOctave<-3) {
+    currentOctave = 3;
+  }
+}
---
+++ 2019/receiver_modular/leds.h
@@ -0,0 +1,47 @@
+void showLeds(){
+switch(currentOctave){
+    case 0: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+            analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+            analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+            analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+    break;
+    case 1: analogWrite(OCTAVE_UP_LED_RED_PIN,0);
+            analogWrite(OCTAVE_UP_LED_GREEN_PIN,0);
+            analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+            analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+    break;
+    case 2: analogWrite(OCTAVE_UP_LED_RED_PIN,0);
+            analogWrite(OCTAVE_UP_LED_GREEN_PIN,75);
+            analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+            analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+    break;
+    case 3: analogWrite(OCTAVE_UP_LED_RED_PIN,0);
+            analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+            analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+            analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+    break;
+    
+    case -1: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+             analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+             analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+             analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,0);
+    break;
+    case -2: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+             analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+             analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+             analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,75);
+    break;
+    case -3: analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+             analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+             analogWrite(OCTAVE_DOWN_LED_RED_PIN,0);
+             analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+    break;
+  }
+}
+
+void TurnOffAll(){
+  analogWrite(OCTAVE_UP_LED_RED_PIN,255);
+  analogWrite(OCTAVE_UP_LED_GREEN_PIN,255);
+  analogWrite(OCTAVE_DOWN_LED_RED_PIN,255);
+  analogWrite(OCTAVE_DOWN_LED_GREEN_PIN,255);
+}
